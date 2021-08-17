90399ca3bcbe2b8837bad1f0690fc50000fc4298
Shubham Gupta <shubhamgmath@gmail.com>
2021-08-17 14:22:52 +0530

    Update custom-shortcode.php
    Added plugin header

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -1,1 +1,15 @@
-
+/**
+ * Plugin Name:       Custom Shortcode
+ * Plugin URI:        https://lazlo.in/plugins/Custom Shortcode/
+ * Description:       Plugin to add custom shortcode to get the oldest five post.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shubham Gupta
+ * Author URI:        https://lazlo.in/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-first-plugin
+ * Domain Path:       /languages
+ */

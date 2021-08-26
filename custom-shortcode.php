f632fdb3a94d14a40b72ef497565b81ca697b843
Shubham Gupta <shubhamgmath@gmail.com>
2021-08-26 14:28:02 +0530

    fixed CS errors.

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:       Custom Shortcode
  * Plugin URI:        https://lazlo.in/plugins/Custom Shortcode/
@@ -14,22 +13,24 @@
  * Update URI:        https://example.com/my-plugin/
  * Text Domain:       my-first-plugin
  * Domain Path:       /languages
+ *
+ * @package plugins
  */
 
-add_shortcode( 'custom_shortcode' , 'custom_shortcode_get_posts');
+add_shortcode( 'custom_shortcode', 'custom_shortcode_get_posts' );
 
-/** function to get 5 oldest post*/
-function custom_shortcode_get_posts(){
+/** Function to get 5 oldest post*/
+function custom_shortcode_get_posts() {
 
-$args = array (
-  'post_status' => 'publish',
-  'post_per_page' => 5,
-  'orderby' => 'post_date',
-  'order' => 'DESC');
+	$args = array(
+		'post_status'   => 'publish',
+		'post_per_page' => 5,
+		'orderby'       => 'post_date',
+		'order'         => 'DESC',
+	);
 
-$latest_post = new WP_Query( $args );
+	$latest_post = new WP_Query( $args );
 
-print_r( $latest_post);
+	print_r( $latest_post );
 
 }
-?>

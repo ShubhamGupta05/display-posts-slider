5f7020d281f0ca013ca43608f318a4451bd4473e
Shubham Gupta <shubhamgmath@gmail.com>
2021-08-31 13:56:26 +0530

    Create a custom shortcode template file

---
+++ template/custom-shortcode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Custom Shortcode template.
+ *
+ * This template is used to show the post.
+ *
+ * @author      Shubham
+ * @package     templates
+ * @version     1.0.0
+ */
+
+?>
+
+<h2><?php echo '$old_post->post_title'; ?></h2>

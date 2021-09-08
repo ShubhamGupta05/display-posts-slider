77ce365b712deff896fad626f505df8c75080164
Shubham Gupta <shubhamgmath@gmail.com>
2021-09-08 14:18:20 +0530

    added comments

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -8,7 +8,7 @@
  * @version 1.0.0
  *
  * Plugin Name:       Custom Shortcode
- * Plugin URI:        https://lazlo.in/plugins/Custom Shortcode/
+ * Plugin URI:        https://lazlo.in/plugins/Custom-Shortcode/
  * Description:       Plugin to add custom shortcode to get the oldest five post.
  * Version:           1.0.0
  * Requires at least: 5.2
@@ -25,7 +25,6 @@
 // adds the custom shortcode.
 add_shortcode( 'custom_shortcode', 'custom_shortcode_get_posts' );
 
-<<<<<<< HEAD
 /**
  * Fetches the oldest 5 posts.
  *
@@ -33,7 +32,7 @@ add_shortcode( 'custom_shortcode', 'custom_shortcode_get_posts' );
  */
 function custom_shortcode_get_posts() {
 
-	// declaring a $args variable and assigning the static values to the properties.
+	// declaring $args variable and assigning the values to the properties.
 	$args = array(
 		'post_status'    => 'publish',
 		'posts_per_page' => 5,
@@ -47,34 +46,6 @@ function custom_shortcode_get_posts() {
 	print_r( $oldest_posts_query->posts );
 
 	// Loops to get post from $old_post.
-=======
-/** 
-*
-*Function to get 5 oldest post.
-*
-*In this function, static arugments are passed.
-*On the basis of those arguments It creates a new query object.
-*Check the array of the post by prinitng the query object posts property.
-*Runs loop to show the each post from the aaray in a proper template.
-*
-*/
-function custom_shortcode_get_posts() {
-	
-	// declaring a $args variable and assigning the static values to the properties. 
-	$args = array(
-		'post_status'   => 'publish',
-		'posts_per_page' => 5,
-		'orderby'       => 'post_date',
-		'order'         => 'DESC',
-	);
-
-	$oldest_posts_query = new WP_Query( $args );
-	
-	//shows an array of all the post.
-	print_r( $oldest_posts_query->posts );
-	
-	//Loops to get post from $old_post
->>>>>>> 1d34e9046cfb32b03c05ca675b1674ee68cd8b15
 	foreach ( $oldest_posts_query->posts as $old_post ) {
 		/**
 		 * Includes the template file.
--- templates/custom-shortcode.php
+++ templates/custom-shortcode.php
@@ -10,13 +10,7 @@
 
 ?>
 <article>
-<<<<<<< HEAD
 	<h2><?php echo esc_html( $old_post->post_title ); ?></h2>
 	<p><?php echo esc_html( $old_post->post_excerpt ); ?></p>
 	<img src="<?php echo get_the_post_thumbnail( $old_post, medium ); ?>" />
-=======
-  <h2><?php echo $old_post->post_title; ?></h2>
-  <p><?php echo $old_post->post_excerpt; ?></p>
-  <img><?php echo get_the_post_thumbnail( $old_post, medium ); ?></img>
->>>>>>> 1d34e9046cfb32b03c05ca675b1674ee68cd8b15
 </article>

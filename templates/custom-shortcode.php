bfcdca5c87e97ae146389f67cd9b5edd4ea0328d
Shubham Gupta <shubhamgmath@gmail.com>
2021-09-09 14:23:01 +0530

    added a function to include CSS file

--- templates/custom-shortcode.php
+++ templates/custom-shortcode.php
@@ -12,5 +12,5 @@
 <article>
 	<h2><?php echo esc_html( $old_post->post_title ); ?></h2>
 	<p><?php echo esc_html( $old_post->post_excerpt ); ?></p>
-	<img src=" <?php echo get_the_post_thumbnail( $old_post, medium ); ?>" />
+	<img src="<?php echo get_the_post_thumbnail( $old_post, medium ); ?>"/>
 </article>

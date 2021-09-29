6299ffd9e05565320f6f2aa71ce2646c5d48f5d2
Shubham Gupta <shubhamgmath@gmail.com>
2021-09-29 14:56:56 +0530

    changed the class name and add styles

--- templates/custom-shortcode.php
+++ templates/custom-shortcode.php
@@ -12,11 +12,9 @@
 // wp_enqueue_style( 'style' );.
 
 ?>
-<article class='article1'>
-	<img class='img1' src="<?php echo esc_html( get_the_post_thumbnail_url( $old_post, 'medium' ) ); ?>"/>
-	<article class='article2'>
-		<h2 class='heading'><?php echo esc_html( $old_post->post_title ); ?></h2>
-		<p class='para1'><?php echo esc_html( $old_post->post_excerpt ); ?></p>
-		<a class='a1' href="<?php echo esc_html( get_permalink( $old_post->ID ) ); ?>" title="Read More">Read More</a>
-	</article>
+<article class='cs_post_template'>
+	<img class='cs_post_thumbnail' src="<?php echo esc_html( get_the_post_thumbnail_url( $old_post, 'medium' ) ); ?>"/>
+	<h2 class='cs_post_title'><?php echo esc_html( $old_post->post_title ); ?></h2>
+	<p class='cs_post_content'><?php echo esc_html( $old_post->post_excerpt ); ?></p>
+	<a class='cs_post_url' href="<?php echo esc_html( get_permalink( $old_post->ID ) ); ?>" title="Read More">Read More</a>
 </article>

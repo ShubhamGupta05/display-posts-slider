093b63f700e519dd4ce39610e6721171380f0752
Shubham Gupta <shubhamgmath@gmail.com>
2021-11-02 14:10:59 +0530

    added error template file

---
+++ templates/error.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Custom Shortcode Error Template.
+ *
+ * @package custom-shortcode/templates
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+
+?>
+<article class='error_template'>
+    <img class='error_img' src="https://www.freeiconspng.com/uploads/error-icon-4.png" width="350" alt="Free High quality Error Icon"/>
+    <h1>An error occured.</h1>
+</article>

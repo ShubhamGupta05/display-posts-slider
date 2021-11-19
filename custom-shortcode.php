bb44be39ed19b1b613b5de72d72685bea06ebd8c
Shubham Gupta <shubhamgmath@gmail.com>
2021-11-19 11:32:44 +0530

    changes due to the plugin name change

--- custom-shortcode.php
+++ custom-shortcode.php
@@ -2,14 +2,14 @@
 /**
  * Main Plugin File
  *
- * @package custom-shortcode
+ * @package display-posts-slider
  *
  * @since 1.0.0
  * @version 1.0.0
  *
- * Plugin Name:       Custom Shortcode
- * Plugin URI:        https://lazlo.in/plugins/Custom-Shortcode/
- * Description:       Plugin to add custom shortcode to get the oldest five post.
+ * Plugin Name:       Display Posts Slider
+ * Plugin URI:        https://lazlo.in/plugins/display-posts-slider/
+ * Description:       Plugin to add custom shortcode to get the your posts in a beautiful slider.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -18,7 +18,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://lazlo.in/my-plugin/
- * Text Domain:       custom-shortcode
+ * Text Domain:       display-posts-slider
  * Domain Path:       /languages
  */
 
@@ -29,25 +29,25 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 0.1.0
  */
-define( 'CS_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'DPS_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 /**
  * Plugin's url path
  *
  * @since 0.1.0
  */
-define( 'CS_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'DPS_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 
 /**
  * The current version
  *
  * @since 0.1.0
  */
-define( 'CS_VERSION', '1.0.0' );
+define( 'DPS_VERSION', '1.0.0' );
 
 /**
  * Includes the main functionality file.
  *
  * @since 1.0.0
  */
-require_once CS_PATH . 'includes/shortcode.php';
+require_once DPS_PATH . 'includes/shortcode.php';
